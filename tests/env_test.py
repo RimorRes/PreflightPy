46b0315330db270b7ad10988609e3eb8c5a04eb2
Oxyde2 <maxime.djomby@gmail.com>
2019-10-28 09:10:25 +0100

    Added Travis CI :tada: and switched unittest with pytest :tada:

--- tests/env_test.py
+++ tests/env_test.py
@@ -1,24 +1,27 @@
-from context import preflight as pre
-import unittest
+from .context import preflight as pre
 
-class TestEnvUpdate(unittest.TestCase):
-
-    def setUp(self):
-        self.parameters = pre.Parameters("tests/input/case.json")
-        self.environment = pre.Environment(self.parameters.env_variables)
-        self.environment.get_status(3048)
+class TestEnvUpdate:
 
     def test_pressure(self):
-        self.assertEqual(round(self.environment.P, -3), 69000)
+        parameters = pre.Parameters("tests/input/case.json")
+        environment = pre.Environment(parameters.env_variables)
+        environment.get_status(3048)
+        assert round(environment.P, -3) == 69000
 
     def test_temperature(self):
-        self.assertEqual(int(self.environment.T), 268)
+        parameters = pre.Parameters("tests/input/case.json")
+        environment = pre.Environment(parameters.env_variables)
+        environment.get_status(3048)
+        assert int(environment.T) == 268
 
     def test_density(self):
-        self.assertEqual(round(self.environment.Rho, 1), 0.9)
+        parameters = pre.Parameters("tests/input/case.json")
+        environment = pre.Environment(parameters.env_variables)
+        environment.get_status(3048)
+        assert round(environment.Rho, 1) == 0.9
 
     def test_speed_of_sound(self):
-        self.assertEqual(int(self.environment.c), 328)
-
-if __name__ == '__main__':
-    unittest.main()
+        parameters = pre.Parameters("tests/input/case.json")
+        environment = pre.Environment(parameters.env_variables)
+        environment.get_status(3048)
+        assert int(environment.c) == 328
