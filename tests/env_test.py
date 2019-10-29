7fbb04a8695721a97f5072f1a46b564c6a976f1d
Oxyde2 <maxime.djomby@gmail.com>
2019-10-29 18:50:23 +0100

    Fixed Environment and Environment test suite :ambulance:

--- tests/env_test.py
+++ tests/env_test.py
@@ -3,34 +3,43 @@ from .context import preflight as pre
 class TestEnvUpdate:
 
     def test_pressure(self):
+        z = lambda h: 6378137*h/(6378137-h)
         parameters = pre.Parameters("tests/input/case.json")
         environment = pre.Environment(parameters.env_variables)
         environment.get_status(5000)
-        assert round(environment.P, -1) == 54050
-        environment.get_status(20000.0)
-        assert round(environment.P, -1) == 5520
-        with pytest.raises(Exception):
-            assert environment.get_status(100000)
+        assert round(environment.P, -1) == 54040
+        environment.get_status(11019)
+        assert round(environment.P, -1) == 22630
+        environment.get_status(20000)
+        assert round(environment.P, -1) == 5530
+        environment.get_status(70000)
+        assert round(environment.P, 1) == 5.2
 
     def test_temperature(self):
         parameters = pre.Parameters("tests/input/case.json")
         environment = pre.Environment(parameters.env_variables)
         environment.get_status(5000)
-        assert round(environment.T, 0) == 256
-        environment.get_status(20000.0)
-        assert round(environment.T, 0) == 217
-        environment.get_status(40000.0)
-        assert round(environment.T, 0) == 250
+        assert round(environment.T, 2) == 255.68
+        environment.get_status(20063)
+        assert round(environment.T, 2) == 216.65
+        environment.get_status(30000)
+        assert round(environment.T, 2) == 226.51
+        environment.get_status(47350)
+        assert round(environment.T, 2) == 270.65
+        environment.get_status(71802)
+        assert round(environment.T, 2) == 214.64
 
     def test_density(self):
         parameters = pre.Parameters("tests/input/case.json")
         environment = pre.Environment(parameters.env_variables)
         environment.get_status(5000)
-        assert round(environment.Rho, 2) == 0.74
-        environment.get_status(20000.0)
-        assert round(environment.Rho, 2) == 0.09
+        assert round(environment.Rho, 4) == 0.7365
+        environment.get_status(11019)
+        assert round(environment.Rho, 4) == 0.3639
+        environment.get_status(20063)
+        assert round(environment.Rho, 4) == 0.0880
         environment.get_status(40000.0)
-        assert round(environment.Rho, 3) == 0.004
+        assert round(environment.Rho, 4) == 0.0040
 
 
     def test_speed_of_sound(self):
