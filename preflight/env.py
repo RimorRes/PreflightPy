b3b3c4c6c3c139e10c884bfc2e18ec3a85ea62e8
Oxyde2 <maxime.djomby@gmail.com>
2019-10-29 09:34:42 +0100

    Fixed temperature model and density formula :ambulance:

--- preflight/env.py
+++ preflight/env.py
@@ -8,12 +8,20 @@ class Environment:
         self.elev, self.t, self.g, self.M_air, self.R, self.gamma, self.Pstatic = vars
 
     def get_temp(self, h: float) -> float:
-        if h <= 11000 :
-            return 15.04 - 0.00649*h + 273.15
-        elif 11000 < h <= 25000 :
-            return -56.46 +273
-        elif h > 25000 :
-            return -131.21 + 0.00299*h + 273.15
+        if 0 <= h < 11000:
+            return 288.15 + (-0.0065*(h-0))
+        elif 11000 <= h < 20000:
+            return 216.65 + (0.0*(h-11000))
+        elif 20000 <= h < 32000:
+            return 216.65 + (0.001*(h-20000))
+        elif 32000 <= h < 47000:
+            return 228.65 + (0.0028*(h-32000))
+        elif 47000 <= h < 51000:
+            return 270.65 + (0.0*(h-47000))
+        elif 51000 <= h < 71000:
+            return 270.65 + (-0.0028*(h-51000))
+        elif 71000 <= h:
+            return 214.65 + (-0.002*(h-71000)) if not 214.65 + (-0.002*(h-71000)) < 0 else 0
 
     def get_pressure(self, h: float, T: float)-> float:
         try:
@@ -22,9 +30,10 @@ class Environment:
             return 0
 
     def get_density(self, P: float, T: float) -> float:
-        N = 1
-        m = self.M_air * N
-        return (P * m )/(N * self.R * T)
+        V = 1
+        n = (P*V)/(self.R*T)
+        m = self.M_air * n
+        return (P * m )/(n * self.R * T)
 
     def get_c(self, T: float):
         return math.sqrt((self.gamma * self.R * T) / self.M_air)
--- tests/env_test.py
+++ tests/env_test.py
@@ -5,23 +5,40 @@ class TestEnvUpdate:
     def test_pressure(self):
         parameters = pre.Parameters("tests/input/case.json")
         environment = pre.Environment(parameters.env_variables)
-        environment.get_status(3048)
-        assert round(environment.P, -3) == 69000
+        environment.get_status(5000)
+        assert round(environment.P, -1) == 54050
+        environment.get_status(20000.0)
+        assert round(environment.P, -1) == 5520
+        with pytest.raises(Exception):
+            assert environment.get_status(100000)
 
     def test_temperature(self):
         parameters = pre.Parameters("tests/input/case.json")
         environment = pre.Environment(parameters.env_variables)
-        environment.get_status(3048)
-        assert int(environment.T) == 268
+        environment.get_status(5000)
+        assert round(environment.T, 0) == 256
+        environment.get_status(20000.0)
+        assert round(environment.T, 0) == 217
+        environment.get_status(40000.0)
+        assert round(environment.T, 0) == 250
 
     def test_density(self):
         parameters = pre.Parameters("tests/input/case.json")
         environment = pre.Environment(parameters.env_variables)
-        environment.get_status(3048)
-        assert round(environment.Rho, 1) == 0.9
+        environment.get_status(5000)
+        assert round(environment.Rho, 2) == 0.74
+        environment.get_status(20000.0)
+        assert round(environment.Rho, 2) == 0.09
+        environment.get_status(40000.0)
+        assert round(environment.Rho, 3) == 0.004
+
 
     def test_speed_of_sound(self):
         parameters = pre.Parameters("tests/input/case.json")
         environment = pre.Environment(parameters.env_variables)
-        environment.get_status(3048)
-        assert int(environment.c) == 328
+        environment.get_status(1000)
+        assert round(environment.c, 1) == 336.4
+        environment.get_status(10000)
+        assert round(environment.c, 1) == 299.5
+        environment.get_status(30000)
+        assert round(environment.c, 1) == 301.7
