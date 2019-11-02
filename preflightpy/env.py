ca40f653e89736acbdc1123887e7b8cd2c54284f
Oxyde2 <maxime.djomby@gmail.com>
2019-11-02 15:03:53 +0100

    Added temperature formulas for 86 - 1000 km :sparkles:

--- preflightpy/env.py
+++ preflightpy/env.py
@@ -33,7 +33,7 @@ class Environment:
     def get_geopotential_altitude(self, r: float, z: float) -> float:
         return r*z / (r+z)
 
-    def get_temp(self, h: float) -> float:
+    def get_temp(self, z: float, h: float) -> float:
         if 0 <= h <= 11000:
             return (288.15 + (self.Lm[0]*(h-0)), 0)
         elif 11000 < h <= 20000:
@@ -46,31 +46,39 @@ class Environment:
             return (270.65 + (self.Lm[4]*(h-47000)), 4)
         elif 51000 < h <= 71000:
             return (270.65 + (self.Lm[5]*(h-51000)), 5)
-        elif 71000 < h:
+        elif 71000 < h <= 84856:
             return (214.65 + (self.Lm[6]*(h-71000)), 6)
+        elif 86000 < z <= 91000:
+            return (186.67, 7)
+        elif 91000 < z <= 110000:
+            return (263.1905 - 76.3232 * math.sqrt(1 - ((z - 91000) / -19942.9)**2), 8)
+        elif 110000 < z <= 120000:
+            return (240 + 0.012 * (z - 110000), 9)
+        elif 120000 < z <= 1000000:
+            xi = (z - 120000) * (6356766 + 120000) / (6356766 + z)
+            return (1000 - 640 * math.exp(-0.00001875 * xi), 10)
 
     def get_pressure(self, h: float, T: float, b: int)-> float:
-        if self.Lm[b] != 0:
-            return self.Pb[b] * ( self.Tb[b]/T )**(self.g*self.M_air/(self.R*self.Lm[b]))
-        else:
-            return self.Pb[b] * math.exp( -self.g * self.M_air * (h-self.hb[b]) / (self.R*self.Tb[b]) )
+        if b <= 6:
+            if self.Lm[b] != 0:
+                return self.Pb[b] * ( self.Tb[b]/T )**(self.g*self.M_air/(self.R*self.Lm[b]))
+            else:
+                return self.Pb[b] * math.exp( -self.g * self.M_air * (h-self.hb[b]) / (self.R*self.Tb[b]) )
 
 
-    def get_density(self, P: float, T: float) -> float:
-        V = 1
-        n = (P*V)/(self.R*T)
-        m = self.M_air * n
-        return (P * m )/(n * self.R * T)
+    def get_density(self, P: float, T: float, b) -> float:
+        if b <= 6:
+            V = 1
+            n = (P*V)/(self.R*T)
+            m = self.M_air * n
+            return (P * m )/(n * self.R * T)
 
     def get_c(self, T: float):
         return math.sqrt((self.gamma * self.R * T) / self.M_air)
 
     def get_status(self, z: float):
-        h = round(self.get_geopotential_altitude(6378137, z),0)
-        print(h)
-        self.T, b = self.get_temp(h)
-        print(self.T, b)
+        h = round(self.get_geopotential_altitude(6356766, z), 0)
+        self.T, b = self.get_temp(z, h)
         self.P = self.get_pressure(h, self.T, b)
-        print(self.P)
-        self.Rho = self.get_density(self.P, self.T)
+        self.Rho = self.get_density(self.P, self.T, b)
         self.c = self.get_c(self.T)
